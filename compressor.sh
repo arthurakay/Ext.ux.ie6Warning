9b07701563a8ea6f452b9699d1460e09488bdb60
Arthur Kay <arthurakay@gmail.com>
2011-07-26 20:05:41 -0500

    Updated combined/minified files.

--- compressor.sh
+++ compressor.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 EXTJS="./ext-4.0.4/ext-all.js"
-IE6WARNING = "Ext.ux.ie6Warning.js"
-
-echo "Combining JS files..."
-
-mkdir tmpjs
-cat $EXTJS $IE6WARNING > ./tmpjs/tmp.js
+IE6WARNING="./Ext.ux.ie6Warning.js"
 
 echo "Removing old minified files..."
 rm ./compressed/*
 
+echo "Combining JS files..."
+cat $EXTJS $IE6WARNING > ./compressed/Ext.ux.ie6Warning-all.js
+
 echo "Running YUI Compressor"
-java -jar ./yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar --type js -o ./compressed/Ext.ux.ie6Warning-all.js ./tmpjs/tmp.js
+java -jar ./yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar --type js -o ./compressed/Ext.ux.ie6Warning-min.js ./compressed/Ext.ux.ie6Warning-all.js
+
+echo "Done!"
